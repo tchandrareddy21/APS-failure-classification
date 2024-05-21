4017ffad9808ff8da72cd36c12cf901ba2cd194a
TIYYAGURA CHANDRA REDDY <chandra.tiyyagura@gmail.com>
2024-05-21 21:41:43 +0530

    Data ingestion test completd

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from sensor.configuration.monog_db_connection import MongoDBClient
 
+from sensor.entity.config_entity import TrainingPipelineConfig, DataIngestionConfig
+from sensor.logger import logging
+from sensor.exception import  SensorException
+
 if __name__ == "__main__":
-    mongodb_client = MongoDBClient()
-    print(f"Collection Name : {mongodb_client.database.list_collection_names()}")
+    training_pipeline_config = TrainingPipelineConfig()
+    data_ingestion_config = DataIngestionConfig(training_pipeline_config= training_pipeline_config)
+    print(data_ingestion_config.__dict__)
