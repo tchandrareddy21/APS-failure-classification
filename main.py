9b0fadb0babdad98f4b4bc72223d0270e986e15f
TIYYAGURA CHANDRA REDDY <chandra.tiyyagura@gmail.com>
2024-05-21 23:08:45 +0530

    data ingestion with error

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from sensor.entity.config_entity import TrainingPipelineConfig, DataIngestionCon
 from sensor.logger import logging
 from sensor.exception import  SensorException
 
+from sensor.pipeline.training_pipeline import TrainPipeline
+
 if __name__ == "__main__":
-    training_pipeline_config = TrainingPipelineConfig()
-    data_ingestion_config = DataIngestionConfig(training_pipeline_config= training_pipeline_config)
-    print(data_ingestion_config.__dict__)
+    train_pipeline = TrainPipeline()
+    train_pipeline.run_pipeline()
