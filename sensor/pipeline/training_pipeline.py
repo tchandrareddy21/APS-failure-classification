f885c6d5ed73656e14c4e820575c855cc3644586
TIYYAGURA CHANDRA REDDY <chandra.tiyyagura@gmail.com>
2024-05-23 12:21:52 +0530

    data validation with one error

--- sensor/pipeline/training_pipeline.py
+++ sensor/pipeline/training_pipeline.py
@@ -1,9 +1,10 @@
-from sensor.entity.config_entity import TrainingPipelineConfig, DataIngestionConfig
+from sensor.entity.config_entity import TrainingPipelineConfig, DataIngestionConfig, DataValidationConfig
 from sensor.exception import SensorException
 import sys, os
 from sensor.logger import logging
-from sensor.entity.artifact_entity import DataIngestionArtifact
+from sensor.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact
 from sensor.components.data_ingestion import DataIngestion
+from sensor.components.data_validation import DataValidation
 
 class TrainPipeline:
     def __init__(self):
@@ -23,9 +24,12 @@ class TrainPipeline:
         except Exception as e:
             raise SensorException(e, sys)
         
-    def start_data_validation(self):
+    def start_data_validation(self, data_ingestion_artifact: DataIngestionArtifact)-> DataValidationArtifact:
         try:
-            pass
+            self.start_data_validation_config = DataValidationConfig(training_pipeline_config= self.training_pipeline_config)
+            data_validation = DataValidation(data_ingestion_artifact=data_ingestion_artifact,
+                                             data_validation_config=DataValidationConfig)
+            data_validation_artifact = data_validation.initiate_data_validation()
         except Exception as e:
             raise SensorException(e, sys)
     
@@ -56,5 +60,6 @@ class TrainPipeline:
     def run_pipeline(self):
         try:
             data_ingestion_artifact: DataIngestionArtifact = self.start_data_ingestion()
+            data_validation_artifact = self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
         except Exception as e:
             raise SensorException(e, sys)
---
+++ sensor/utils/main_utils.py
@@ -0,0 +1,73 @@
+from sensor.exception import SensorException
+from sensor.logger import logging
+import yaml
+import sys, os, dill
+import numpy as np
+
+def read_yaml_file(file_path: str) -> dict:
+    try:
+        with open(file_path, "rb") as yaml_file:
+            return yaml.safe_load(yaml_file)
+    except Exception as e:
+        raise SensorException(e, sys) from e
+    
+
+def write_yaml_file(filepath: str, content: object, replace: bool = False)-> None:
+    try:
+        if replace:
+            if os.path.exists(filepath):
+                os.remove(filepath)
+        os.makedirs(os.path.dirname(filepath), exist_ok= True)
+        with open(filepath, "w") as file:
+            yaml.dump(content, file)
+    except Exception as e:
+        raise SensorException(e, sys) from e
+    
+
+def save_numpy_array_data(file_path: str, array: np.array):
+    """
+    Save numpy array data to file
+    file_path: str location of file to save
+    array: np.array data to save
+    """
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+        with open(file_path, "wb") as file_obj:
+            np.save(file_obj, array)
+    except Exception as e:
+        raise SensorException(e, sys) from e
+
+
+def load_numpy_array_data(file_path: str) -> np.array:
+    """
+    load numpy array data from file
+    file_path: str location of file to load
+    return: np.array data loaded
+    """
+    try:
+        with open(file_path, "rb") as file_obj:
+            return np.load(file_obj)
+    except Exception as e:
+        raise SensorException(e, sys) from e
+
+
+def save_object(file_path: str, obj: object) -> None:
+    try:
+        logging.info("Entered the save_object method of MainUtils class")
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        with open(file_path, "wb") as file_obj:
+            dill.dump(obj, file_obj)
+        logging.info("Exited the save_object method of MainUtils class")
+    except Exception as e:
+        raise SensorException(e, sys) from e
+
+
+def load_object(file_path: str, ) -> object:
+    try:
+        if not os.path.exists(file_path):
+            raise Exception(f"The file: {file_path} is not exists")
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
+    except Exception as e:
+        raise SensorException(e, sys) from e
