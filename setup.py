e88f835027b01f975ab59d34e03f1d7c68629035
Tiyyagura Chandra Reddy <tchandrareddy21@gmail.com>
2025-02-17 15:11:17 +0530

    setup.py updated

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 from typing import List
 
-HYPEN_E_DOT = "-e ."
+HYPHEN_E_DOT = "-e ."
 
 def get_requirements(file_path: str) -> List[str]:
     """
@@ -9,18 +9,17 @@ def get_requirements(file_path: str) -> List[str]:
     """
     requirements = []
     with open(file_path) as file_obj:
-        requirements = file_obj.readlines()
-        requirements = [req.replace("\n","") for req in requirements]
-        if HYPEN_E_DOT in requirements:
-            requirements.remove(HYPEN_E_DOT)
+        requirements = file_obj.read().splitlines()
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
         return requirements
     
 
 setup(
     name = "Sensor-Fault-Detection",
     version= "0.0.1",
-    author= "Chandra Reddy Tiyyagura",
-    author_email= "chandra.tiyyagura@gmail.com",
+    author= "Tiyyagura Chandra Reddy",
+    author_email= "tchandrareddy21@gmail.com",
     install_requires = get_requirements('requirements.txt'),
     packages= find_packages()
 )
